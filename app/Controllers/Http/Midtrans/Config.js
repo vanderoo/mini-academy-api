743b4c02688851b704c4357c8e1882e18e95aee6
Evander Tokang <102147928+vanderoo@users.noreply.github.com>
2023-05-29 09:06:28 +0700

    Update Config.js

--- app/Controllers/Http/Midtrans/Config.js
+++ app/Controllers/Http/Midtrans/Config.js
@@ -3,16 +3,16 @@
 const SANDBOX_BASE_URL = "https://api.sandbox.midtrans.com/v2";
 const PRODUCTION_BASE_URL = "https://api.midtrans.com/v2";
 
-class ConfigController {
+class Config {
     static serverKey = 'server key';
     static isProduction = false;
     static is3ds = false;
     static isSanitized = false;
 
     static getBaseUrl(){
-        return ConfigController.isProduction ? PRODUCTION_BASE_URL : SANDBOX_BASE_URL;
+        return Config.isProduction ? PRODUCTION_BASE_URL : SANDBOX_BASE_URL;
     }
     
 }
 
-module.exports = ConfigController
+module.exports = Config
