30689518448c9731928c1ef59bb41e17b66f41a3
Evander Tokang <102147928+vanderoo@users.noreply.github.com>
2023-05-29 09:12:37 +0700

    Update Config.js

--- app/Controllers/Http/Midtrans/Config.js
+++ app/Controllers/Http/Midtrans/Config.js
@@ -4,7 +4,7 @@ const SANDBOX_BASE_URL = "https://api.sandbox.midtrans.com/v2";
 const PRODUCTION_BASE_URL = "https://api.midtrans.com/v2";
 
 class Config {
-    static serverKey = 'SB-Mid-server-StKVTGNGcmb-zLXDpKfO0tJM';
+    static serverKey = 'server key';
     static isProduction = false;
     static is3ds = false;
     static isSanitized = false;
