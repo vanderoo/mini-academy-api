8ea8ef679651b290834ac6a4b585007a87755212
Evander Tokang <vandertokang17@gmail.com>
2023-05-31 14:13:31 +0700

    ;

--- start/kernel.js
+++ start/kernel.js
@@ -14,6 +14,8 @@ const Server = use('Server')
 */
 const globalMiddleware = [
   'Adonis/Middleware/BodyParser',
+  'Adonis/Middleware/Session',
+  'Adonis/Middleware/AuthInit',
   'App/Middleware/ConvertEmptyStringsToNull'
 ]
 
