79186093771ad114a8850e96cf016f983954f050
Evander Tokang <vandertokang17@gmail.com>
2023-06-05 15:48:06 +0700

    ina

--- app/Controllers/Http/Midtrans/NotificationController.js
+++ app/Controllers/Http/Midtrans/NotificationController.js
@@ -1,32 +1,32 @@
 'use strict';
 
-const Order = use('App/Models/Order');
+const Pembayaran = use('App/Models/Pembayaran');
 
 class NotificationController {
   async post({ request, response }) {
     try {
       const { order_id, status_code } = request.post();
-      const order = await Order.query()
-        .where('invoice', order_id)
+      const pembayaran = await Pembayaran.query()
+        .where('order_id', order_id)
         .first();
 
-      if (!order) {
+      if (!pembayaran) {
         return response.status(404).send({ code: 0, message: 'Terjadi kesalahan | Pembayaran tidak valid' });
       }
 
       switch (status_code) {
         case '200':
-          order.status = 'SUCCESS';
+          pembayaran.status = 'SUCCESS';
           break;
         case '201':
-          order.status = 'PENDING';
+          pembayaran.status = 'PENDING';
           break;
         case '202':
-          order.status = 'CANCEL';
+          pembayaran.status = 'CANCEL';
           break;
       }
 
-      await order.save();
+      await pembayaran.save();
       return response.status(200).send('Ok');
     } catch (error) {
       return response.status(500).send('Error');
